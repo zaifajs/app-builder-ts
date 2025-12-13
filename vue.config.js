541a2bb5c86cfdd68b26acbeb366be11929dcb1b
zaifajs <huzaifa.wp@gmail.com>
2025-12-13 23:35:23 +0000

    Fix build errors and add docs folder for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/app-builder-ts/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config.plugins.delete('fork-ts-checker')
+  }
 }
 
